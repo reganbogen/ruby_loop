93903f2d41e83771416583f68e0b59d67c0d9d79
Regan Layman <regan.layman@gmail.com>
2020-08-10 15:24:58 -0400

    commented out the loop and added in the ruby each iteration in two ways

--- each_iteration.rb
+++ each_iteration.rb
@@ -1,8 +1,15 @@
 array = [ 0, 1, 2, 4, 5 ]
-i = 0
-while i < array.length
-  item = array[i]
-  puts "The current array item is: #{item}"
-  i += 1
+#i = 0
+#while i < array.length
+#  item = array[i]
+#  puts "The current array item is: #{item}."
+#  i += 1
+#end
+
+array.each do |item|
+  puts "The current array item is: #{item}."
 end
 
+#OR
+
+array.each {|item| puts "The current array item is: #{item}." }
