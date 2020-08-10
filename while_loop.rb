3d5d75795e6bf618404b57c290ba3d49458cb37a
Regan Layman <regan.layman@gmail.com>
2020-08-10 15:05:41 -0400

    added comment descriptions for while loop details

--- while_loop.rb
+++ while_loop.rb
@@ -1,5 +1,5 @@
-answer = ""
-while answer != "n"
+answer = "" #empty string
+while answer != "n" #does not equal the letter 'n'
   print "Do you want me to repeat this pointless loop again? (y/n) "
   answer = gets.chomp.downcase
-end
+end #as soon as condition is met, it will exit or BREAK the loop
