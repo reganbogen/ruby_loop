1bea0b211dc6d02817249b07e2ae6cb6fa74214f
Regan Layman <regan.layman@gmail.com>
2020-08-10 15:34:30 -0400

    added in each_key and each_value examples

--- hash_iteration.rb
+++ hash_iteration.rb
@@ -9,3 +9,15 @@ end
 business.each_pair do |key, value|
   puts "The hash key is #{key} and the value is #{value}."
 end
+
+#OR 
+
+business.each_key do |key|
+  puts "Key: #{key}"
+end
+
+#OR
+
+business.each_value do |value|
+  puts "Key: #{value}"
+end
